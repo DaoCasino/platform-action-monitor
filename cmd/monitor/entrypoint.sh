0ec44f608dd14099e6ce719b7104feae192dc1bf
Algys Ievlev <algys.ievlev@gmail.com>
2020-07-22 18:08:39 +0900

    add history cleanup option

--- cmd/monitor/entrypoint.sh
+++ cmd/monitor/entrypoint.sh
@@ -1,4 +1,20 @@
 #!/bin/sh
+
+set -e
+
+# optional cleanup of history db migrations before run
+if [[ $CLEANUP_HISTORY != "" ]]; then
+    echo "Cleanning daobet history migrations"
+    dbmate down || true
+fi
+
+# run daobet history db mirgations
+echo "Running daobet history migrations"
 dbmate up
+
+# run action monitor db migrations
+echo "Running action monitor migrations"
 dbmate -e SHARED_DATABASE_URL -d /app/shared-db/migrations up
-./monitor -config /app/configs/config.yml
+
+# run action monitor
+exec ./monitor -config /app/configs/config.yml
